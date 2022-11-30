64e6c997c356399cbb4c8282cfcfd5b2e26faf28
dodoringring <dohee1504@gmail.com>
2022-11-30 12:45:41 +0900

    Hello 소스 수정 완료

--- app/src/main/java/dev_java/ch01/Hello.java
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");
-       //개발환경설정
+        // 개발환경설정
+        System.out.println("git 설정 추가");
     }
-    
+
 }
