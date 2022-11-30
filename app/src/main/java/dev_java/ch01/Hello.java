3787f71cf8d289c5d052163f2f03756b834c54da
dodoringring <dohee1504@gmail.com>
2022-11-30 15:41:47 +0900

    11/30일 완료

--- app/src/main/java/dev_java/ch01/Hello.java
+++ app/src/main/java/dev_java/ch01/Hello.java
@@ -2,10 +2,17 @@ package dev_java.ch01;
 
 public class Hello {
 
+    int methodA(int i, int j) {
+        int hap;
+        hap = i + j;
+        return hap;
+    }
+
     public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.methodA(2, 3);
         System.out.println("Hello Java");
         // 개발환경설정
         System.out.println("git 설정 추가");
     }
-
 }
