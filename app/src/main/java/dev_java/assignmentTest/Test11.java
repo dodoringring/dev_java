ba787de163055056c73fee018fbeb0cd81d819f5
dodoringring <dodohee1504@gmail.com>
2022-12-09 10:34:14 +0900

    테스트

---
+++ app/src/main/java/dev_java/assignmentTest/Test11.java
@@ -0,0 +1,12 @@
+package dev_java.assignmentTest;
+
+public class Test11 {
+  public static void main(String[] args) {
+    short s=2;
+    int i=3;
+    int x=s+i;
+    double d =i;
+    System.out.println(d);
+  }
+  
+}
