40d9574d1c227626c0f2a5ba4076cfbde65323ee
김도희 <doheekim@dodoring-MacBookPro.local>
2022-12-23 00:46:58 +0900

    12/22 수정완료

--- app/src/main/java/dev_java/week4/DeptTable7.java
+++ app/src/main/java/dev_java/week4/DeptTable7.java
@@ -13,35 +13,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
 
-public class DeptTable7 extends JFrame implements ActionListener{
+public class DeptTable7 extends JFrame implements ActionListener {
   // 선언부
-  //파라미터의 this는 DeptTable7타입이고 main에서 호출된 생성자로
-  //현재 메모리에 로딩 중인 그 객체를 가리킨다.
-  //this를 왜 넘겨야 하나요? 입력할건데
-  //JTable7Dialog에서 부서번호, 부서명, 지역을 입력한 후 저장버튼을 누르면
-  //Vector<String[]>추가하고 추가된 로우를 포함하는  Vector\가 부모창에 새로 리플레쉬되어야함
-  //리플레쉬 즉, 재사용 되어야 함-이건 부모 클래스에 구현함
-  JTable7Dialog jtd7=new JTable7Dialog(this);
-  String header[] = { "부서번호", "부서명", "지역" }; //표에서 제일 위에를 나타내줄
+  // 파라미터의 this는 DeptTable7타입이고 main에서 호출된 생성자로
+  // 현재 메모리에 로딩 중인 그 객체를 가리킨다.
+  // this를 왜 넘겨야 하나요? 입력할건데
+  // JTable7Dialog에서 부서번호, 부서명, 지역을 입력한 후 저장버튼을 누르면
+  // Vector<String[]>추가하고 추가된 로우를 포함하는 Vector\가 부모창에 새로 리플레쉬되어야함
+  // 리플레쉬 즉, 재사용 되어야 함-이건 부모 클래스에 구현함
+  JTable7Dialog jtd7 = new JTable7Dialog(this);
+  String header[] = { "부서번호", "부서명", "지역" }; // 표에서 제일 위에를 나타내줄
   String datas[][] = new String[1][3]; //
   DefaultTableModel dtm_dept = new DefaultTableModel(datas, header); // 생성자 호출
   JTable jtb_dept = new JTable(dtm_dept);
   JScrollPane jsp_dept = new JScrollPane(jtb_dept, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
       JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-  JPanel jp_north=new JPanel();
-  //부서목록 조회에 들어갈 샘플 데이터 생성
+  JPanel jp_north = new JPanel();
+  // 부서목록 조회에 들어갈 샘플 데이터 생성
   JButton jbtn_sel = new JButton("조회");
   JButton jbtn_ins = new JButton("입력");
   JButton jbt_upd = new JButton("수정");
   JButton jbtn_del = new JButton("삭제");
   JButton jbtn_det = new JButton("상세보기");
 
-static Vector<String[]> vdata=new Vector<>();//vdata.size()=0; 
-String[][] depts = {
-  {"10","개발부","서울"},
-  {"20","인사부","경기"},
-  {"30","총무부","부산"}
-};
+  static Vector<DeptVO> vdata = new Vector<>();// vdata.size()=0;
+  String[][] depts = {
+      { "10", "개발부", "서울" },
+      { "20", "인사부", "경기" },
+      { "30", "총무부", "부산" }
+  };
+
   // 생성자
   public DeptTable7() {
     initDisplay();
@@ -59,16 +60,16 @@ String[][] depts = {
     this.setTitle("부서관리 시스템 Ver1.0");
     this.setSize(500, 400);
     this.setVisible(true);
-    jbtn_ins.setBackground(new Color(20,20,30));
-    jbtn_ins.setForeground(new Color(225,225,225));
-    jbtn_sel.setBackground(new Color(30,60,30));
-    jbtn_sel.setForeground(new Color(225,225,225));
-    jbt_upd.setBackground(new Color(40,100,30));
-    jbt_upd.setForeground(new Color(225,225,225));
-    jbtn_del.setBackground(new Color(50,150,30));
-    jbtn_del.setForeground(new Color(225,225,225));
-    jbtn_det.setBackground(new Color(50,150,30));
-    jbtn_det.setForeground(new Color(225,225,225));
+    jbtn_ins.setBackground(new Color(20, 20, 30));
+    jbtn_ins.setForeground(new Color(225, 225, 225));
+    jbtn_sel.setBackground(new Color(30, 60, 30));
+    jbtn_sel.setForeground(new Color(225, 225, 225));
+    jbt_upd.setBackground(new Color(40, 100, 30));
+    jbt_upd.setForeground(new Color(225, 225, 225));
+    jbtn_del.setBackground(new Color(50, 150, 30));
+    jbtn_del.setForeground(new Color(225, 225, 225));
+    jbtn_det.setBackground(new Color(50, 150, 30));
+    jbtn_det.setForeground(new Color(225, 225, 225));
     jp_north.setLayout(new FlowLayout(FlowLayout.RIGHT));
     jp_north.add(jbtn_ins);
     jp_north.add(jbtn_sel);
@@ -76,66 +77,76 @@ String[][] depts = {
     jp_north.add(jbtn_del);
     jp_north.add(jbtn_det);
     this.add("North", jp_north);
-    this.add("Center",jsp_dept); //디폴트 테이블~테이블을 붙인 스크롤팬을 소환해서 기본테이블을 만들었ㄷ  
+    this.add("Center", jsp_dept); // 디폴트 테이블~테이블을 붙인 스크롤팬을 소환해서 기본테이블을 만들었ㄷ
   }
-  
-  //새로고침
-  //입력, 수정화면에서 저장 버튼 누르면 Vector에 String[]추가하고
-  //그 다이얼 로그 화면은 닫히고 부모창은 새로고침 처리 한다.
-  //그러니까 다이얼로그 창에서 부모 클래스에 리프레쉬데이터메소드를 호출해야한다.
-  //그러니까 인스턴스화 할 때 파라미터에 this를 넘겨서 사용할 수 있도록 할 것(N.P.E안일어나게)
-  public void refreshData(){
-    System.out.println("refreshData 호출");  
-    //입력, 수정 전에 조회된 정보는 삭제함
-    while(dtm_dept.getRowCount()>0){
+
+  // 새로고침
+  // 입력, 수정화면에서 저장 버튼 누르면 Vector에 String[]추가하고
+  // 그 다이얼 로그 화면은 닫히고 부모창은 새로고침 처리 한다.
+  // 그러니까 다이얼로그 창에서 부모 클래스에 리프레쉬데이터메소드를 호출해야한다.
+  // 그러니까 인스턴스화 할 때 파라미터에 this를 넘겨서 사용할 수 있도록 할 것(N.P.E안일어나게)
+  public void refreshData() {
+    System.out.println("refreshData 호출");
+    // 입력, 수정 전에 조회된 정보는 삭제함
+    while (dtm_dept.getRowCount() > 0) {
       dtm_dept.removeRow(0);
     }
-    if(DeptTable7.vdata.size() <=0){
+    if (DeptTable7.vdata.size() <= 0) {
       JOptionPane.showMessageDialog(this, "조회결과가 없습니다.", "Warning", JOptionPane.WARNING_MESSAGE);
-      return;//refresh탈출
+      return;// refresh탈출
     }
-    System.out.println("DeptTable7 : "+vdata.size());
-    //벡터의 크기만큼 반복하면서 dtm_dept데이터셋에 String[]추가함
-    for(int i=0;i<vdata.size();i++){
-      String[] oneRow =vdata.get(i);////들어간는데 버튼누르
-      dtm_dept.addRow(oneRow);
+    System.out.println("DeptTable7 : " + vdata.size());
+    // 벡터의 크기만큼 반복하면서 dtm_dept데이터셋에 String[]추가함
+    for (int i = 0; i < vdata.size(); i++) {
+      DeptVO oneRow = vdata.get(i);//// 들어간는데 버튼누르
+      Vector<Object> vone = new Vector<>();
+      vone.add(oneRow.getDeptno());
+      vone.add(oneRow.getDname());
+      vone.add(oneRow.getloc());
+      dtm_dept.addRow(vone);
     }
   }
+
   @Override
   public void actionPerformed(ActionEvent e) {
-    Object obj=e.getSource();
-    //조회 할거니?
-    if(obj==jbtn_sel){
+    Object obj = e.getSource();
+    // 조회 할거니?
+    if (obj == jbtn_sel) {
       // dtm_dept.setDataVector(depts, header);//조회 누르면 데이터가 표가 들어감
-     refreshData();
+      refreshData();
     }
 
-    //너입력 버튼 누른거야?
-    if(obj == jbtn_ins){
-      jtd7.set("입력",true,null);
+    // 너입력 버튼 누른거야?
+    if (obj == jbtn_ins) {
+      jtd7.set("입력", true, null, true);
     }
-    //너 수정할려고?
-    else if(obj==jbt_upd){
-      //어떤 로우를 수정할거니?
-      //JTable목록에서 선택한 로우의 인덱스값을 가져옴
-      int index=jtb_dept.getSelectedRow();//테이블에서 선택된 열의 번호를 가져오는것
+    // 너 수정할려고?
+    else if (obj == jbt_upd) {
+      // 어떤 로우를 수정할거니?
+      // JTable목록에서 선택한 로우의 인덱스값을 가져옴
+      int index = jtb_dept.getSelectedRow();// 테이블에서 선택된 열의 번호를 가져오는것
       // dtm_dept.removeRow(index);
-      //데이셋객체로 백터를 사용중이니 벡터에서 꺼낸 값을 String[]초기화
-      //테이블 양식 폼인 JTable이벤트로 얻어옴
-      String[] oneRow=vdata.get(index);//한줄씩 꺼내오기
-      jtd7.set("수정",true,oneRow);
+      // 데이셋객체로 백터를 사용중이니 벡터에서 꺼낸 값을 String[]초기화
+      // 테이블 양식 폼인 JTable이벤트로 얻어옴
+      DeptVO pdVO = vdata.get(index);// 한줄씩 꺼내오기
+      jtd7.set("수정", true, pdVO, true);
     }
-    //너 상세보기원해?
-    else if(obj ==jbtn_det){
-      int index=jtb_dept.getSelectedRow();
-      String[] oneRow=vdata.get(index);
-      jtd7.set("상세보기",true,oneRow);
+    // 너 상세보기원해?
+    else if (obj == jbtn_det) {
+      int index = jtb_dept.getSelectedRow();
+      if (index == 1) {
+        JOptionPane.showMessageDialog(this, "상세보기 할 것을 선택해줘");
+        return;// actionPerformd탈출함
+      }
+      // 벡터안에 사용자가 선택한 DeptVO를 찾아야 하니까.... 그러니까 선택하라구
+
+      DeptVO pdVO = vdata.get(index);
+      jtd7.set("상세보기", true, pdVO, false);
     }
-    
+
   }
+
   public static void main(String[] args) {
     new DeptTable7();
   }
 }
-  
-
--- app/src/main/java/dev_java/week4/JTable7Dialog.java
+++ app/src/main/java/dev_java/week4/JTable7Dialog.java
@@ -17,18 +17,20 @@ public class JTable7Dialog extends JDialog implements ActionListener{
    // 선언부
    DeptTable7 dept7=null;
    JPanel jpc = new JPanel();
-   JScrollPane jsp = new JScrollPane(jpc); // 지금은 예쁘라고 붙임
+   JScrollPane jsp = new JScrollPane(jpc); 
    JPanel jps = new JPanel(); // ->저장 / 닫기 용
-   JLabel jl1 = new JLabel("부서번호");
+   JLabel jlb_deptno = new JLabel("부서번호");
    JTextField jtf_deptno = new JTextField(10);
-   JLabel jl2 = new JLabel("부서명");
+   JLabel jlb_dname = new JLabel("부서명");
    JTextField jtf_dname = new JTextField(20);
-   JLabel jl3 = new JLabel("지역");
+   JLabel jlb_loc = new JLabel("지역");
    JTextField jtf_loc = new JTextField(20);
    // jps속지
    JButton jbtn_save = new JButton("저장");
    JButton jbtn_close = new JButton("닫기");
-    String[] oneRow=null;
+   JButton jbtn_sample =new JButton("자동입력");
+   JButton jbtn_check =new JButton("중복체크");
+    DeptVO pdVO=null;
    // 생성자
    public JTable7Dialog() {
    }
@@ -43,24 +45,29 @@ public class JTable7Dialog extends JDialog implements ActionListener{
    public void initDisplay() {
     jbtn_save.addActionListener(this);
 		jbtn_close.addActionListener(this);
+    jbtn_sample.addActionListener(this);
+		jbtn_check.addActionListener(this);
      this.setSize(400, 360);
      jpc.setLayout(null);
      jps.setLayout(new FlowLayout(FlowLayout.RIGHT));
-     jl1.setBounds(20, 20, 50, 20);
+     jlb_deptno.setBounds(20, 20, 50, 20);
      jtf_deptno.setBounds(70, 20, 100, 20);
-     jl2.setBounds(170, 20, 40, 20);
+     jlb_dname.setBounds(170, 20, 40, 20);
      jtf_dname.setBounds(210, 20, 100, 20);
-     jl3.setBounds(310, 20, 30, 20);
+     jlb_loc.setBounds(310, 20, 30, 20);
      jtf_loc.setBounds(340, 20, 100, 20);
-     jpc.add(jl1);
-     jpc.add(jl2);
-     jpc.add(jl3);
+     jpc.add(jlb_deptno);
+     jpc.add(jlb_dname);
+     jpc.add(jlb_loc);
      jpc.add(jtf_deptno);
      jpc.add(jtf_dname);
      jpc.add(jtf_loc);
+     jpc.add(jbtn_check);
      jps.add(jbtn_save);
      jps.add(jbtn_close);
+     jps.add(jbtn_sample);
      jsp.add(jpc);
+
      this.add("Center", jpc);
      this.add("South", jps);
      this.setVisible(false);
@@ -94,26 +101,35 @@ public class JTable7Dialog extends JDialog implements ActionListener{
   //actionPerformed에서 이벤트(입력,수정,상세보기)가 발생되면 호출됨
 	//메소드의 파라미터 자리는 Call by Value에 의해서 결정됨
 
-   public void set(String title, boolean isView, String[] oneRow){
+  public void set(String title, boolean isView, DeptVO pdVO, boolean isEdit){
     this.setTitle(title);
     this.setVisible(isView);
-    this.oneRow=oneRow;
-    setValue(oneRow);//?????초기화
+    this.pdVO=pdVO;
+    setValue(pdVO);//?????초기화
+    setEditable(isEdit);
   }
 
-  public void setValue(String[] oneRow){
+  // 입력 혹은 수정일 때는 true로 처리하고
+	// 상세보기일 때는 false를 주어서 얼림
+	private void setEditable(boolean isEdit) {
+		jtf_deptno.setEditable(isEdit);
+		jtf_dname.setEditable(isEdit);
+		jtf_loc.setEditable(isEdit);
+	}
+
+  public void setValue(DeptVO pdVO){
   //입력을 위한 윈도우 설정-모든 값을 빈문자열로 셋팅함
-    if(oneRow==null){
+    if(pdVO==null){
       setDeptno("");
       setDname("");
       setloc("");
     }
   //상세조회, 수정시는 배열로 받은 값으로 셋팅함
+  // 부모창에서 set메소드 호출시 파라미터로 넘겨준 값으로 초기화할것.
     else{
-      setDeptno(oneRow[0]);
-      setDname(oneRow[1]);
-      setloc(oneRow[2]);
-    
+      setDeptno(String.valueOf(pdVO.getDeptno()));
+      setDname(pdVO.getDname());
+      setloc(pdVO.getLoc());
     }
   }//end of setValue
   public static void main(String[] args) {
@@ -124,21 +140,44 @@ public class JTable7Dialog extends JDialog implements ActionListener{
   public void actionPerformed(ActionEvent e) {
     Object obj=e.getSource();
     if(obj==jbtn_save){
-      //oneRow가 존재하면 수정모드, 그렇지 않으면 입력모드로 함
-      if(oneRow!=null){// 
-        //날리고 추가하기?
-        // .remove
-      }else{//그외에는 
-        String[] oneRow={getDeptno(),getDname(),getloc()};
-        System.out.println(oneRow[0]+","+oneRow[1]+","+oneRow[2]);
-        System.out.println("before"+DeptTable7.vdata.size());
-        dept7.vdata.add(oneRow);
-        System.out.println("after"+DeptTable7.vdata.size());
-        this.dispose();
-      }
-    }
-    
-    
-    
-  }
+      // oneRow가 존재하면 수정모드, 그렇지 않으면 입력모드로 함
+			// 다이얼그화면에서 저장버튼 하나로 어떨때는 입력처리를 어떤경우는 수정처리를 한다.
+			// 어떻게 나누지? 부모창에서 버튼이 눌려졌을 때 set메소드를 호출하는데
+			// 이 때 입력이면 세번째 파라미터에 null을 입력하고 만일 수정이라면
+			// JTable에서 선택된 로우의 인덱스로 DeptVO를 추출해서 세번째 파라미터에 null대신
+			// 입력해줌(내안에 수정하고자 하는 로우의 DeptVO가 있고 그 안에는 deptno, dname, loc가 있다.)
+			if (pdVO != null) {
+				// 처음에는 벡터에 Generic으로 String[]사용하였으므로 2중 for문을 사용해야 했음
+				// 그런데 지금 DeptVO로 변경했으니까. for문 하나면 됨
+				for (int i = 0; i < DeptTable7.vdata.size(); i++) {
+					DeptVO comVO = DeptTable7.vdata.get(i);
+					// 부모창(DeptTable7)에서 (set메소드 세번째)받아온 부서번호와
+					// 벡터에서 꺼낸 DeptVO의 부서번호가 같니?
+					if (pdVO.getDeptno() == comVO.getDeptno()) {//
+						// 사용자가 입력한 부서정보를 새로 가져와서 DeptVO에 담기
+						DeptVO updVO = DeptVO.builder().deptno(Integer.parseInt(getDeptno())).dname(getDname())
+								.loc(getloc()).build();
+						DeptTable7.vdata.remove(i);
+						DeptTable7.vdata.add(i, updVO);
+						break;
+					}
+				} ////////// end of for
+				dept7.refreshData();
+				this.dispose();
+			} else {
+				DeptVO insVO = DeptVO.builder().deptno(Integer.parseInt(getDeptno())).dname(getDname()).loc(getLoc())
+						.build();
+				System.out.println("before : " + DeptTable7.vdata.size());
+				DeptTable7.vdata.add(insVO);
+				System.out.println("after : " + DeptTable7.vdata.size());
+				dept7.refreshData();
+				this.dispose();
+			}
+		} // end of save
+		else if (obj == jbtn_sample) {
+			setDeptno("30");
+			setDname("운영부");
+			setloc("세종");
+		}
+	}
 }
